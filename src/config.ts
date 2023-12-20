0c74d98e5224d2d6695b6e359f942a3f6b899278
Elias Jackson <ejthedj0@gmail.com>
2023-12-19 16:16:25 -0800

    Post every hour for testing

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,8 @@ export default {
 			name: 'News',
 			frequency: {
 				type: 'perDay',
-				hours: [12],
+				// hours: [7, 12],
+				hours: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
 			},
 			preferredDomains: ['apnews.com', 'reuters.com'],
 		},
@@ -60,7 +61,8 @@ export default {
 			name: 'Opinion',
 			frequency: {
 				type: 'perDay',
-				hours: [7],
+				// hours: [7],
+				hours: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
 			},
 		},
 	] as Array<RedditConfig>,
