f903ea1cc369dbb4034f15031f32a43123cf6103
Elias Jackson <ejthedj0@gmail.com>
2023-12-20 00:57:09 -0800

    MockRedditClient

--- src/services/reddit/reddit.ts
+++ src/services/reddit/reddit.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { RedditChild, RedditResponseData } from './reddit-response';
+import { RedditChild } from './interfaces/reddit-response';
+import redditTestData from '../../services/test-data/reddit.json';
 
 export interface RedditClientInterface {
 	getPosts(subreddits: Array<string>): Promise<Array<RedditChild>>;
@@ -9,6 +10,14 @@ export class RedditClient implements RedditClientInterface {
 	public async getPosts(subreddits: Array<string>) {
 		const response = await axios.get(`https://www.reddit.com/r/${subreddits.join('+')}/top.json?t=day`);
 		const posts = response.data.data.children;
+		return posts;
+	}
+}
+
+export class MockRedditClient implements RedditClientInterface {
+	public async getPosts(_subreddits: Array<string>) {
+		const response = redditTestData;
+		const posts = response.data.children as Array<RedditChild>;
 		return posts;
 	}
 }
