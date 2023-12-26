37103c636efe4f8903d72236e70df3d099638200
Elias Jackson <ejthedj0@gmail.com>
2023-12-26 07:07:52 -0800

    Fix log

--- src/startup/loaders.ts
+++ src/startup/loaders.ts
@@ -60,7 +60,7 @@ export async function loadStructures<T>(
 		// If the structure is a valid structure, add it
 		if (predicate(structure)) structures.push(structure);
 
-		console.log(`Loaded ${new URL(`${dir}/${file}`.replace(`file:///${__dirname}/src`, '..'))}`);
+		console.log(`Loaded ${dir}/${file}`);
 	}
 
 	return structures;
